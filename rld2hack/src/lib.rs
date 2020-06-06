342d6ab69c758912a4a833e5f8612d43f55c60d9
Riley Labrecque <git@rileylabrecque.com>
2020-06-06 15:07:28 -0700

    Started fleshing out d2hack

---
+++ rld2hack/src/lib.rs
@@ -0,0 +1,83 @@
+/*
+    Rust code explaining how direct function detours work
+*/
+#![cfg(all(windows, feature = "nightly"))]
+
+use kernel32;
+//use detour::static_detour;
+use winapi::{HWND, LPCSTR, UINT, c_int};
+//use lazy_static;
+
+/*type createmove_fn = fn(f32, *mut UserCmd) -> bool;
+
+struct UserCmd {
+    /* dscode here */
+}
+
+struct FunctionPtrAddress {
+    addy: createmove_fn
+}
+
+lazy_static! {
+    static ref fn_ptrs: FunctionPtrAddress = FunctionPtrAddress {
+        addy: unsafe {
+            std::mem::transmute::<usize, createmove_fn>(0x10111790)
+        }
+    };
+}
+
+static_detour! {
+    struct CreateMoveDetour: fn(f32, *mut UserCmd) -> bool;
+}
+
+fn createmove_hook(input_sample_time: f32, cmd: *mut UserCmd) -> bool {
+    println!("original function");
+
+    return (fn_ptrs.addy)(input_sample_time, cmd);
+}*/
+
+fn init() {
+    unsafe {
+        kernel32::AllocConsole()
+    };
+
+    println!("Initializing...");
+
+    /*let closure_for_createmove = |input_sample_time, cmd| {
+        println!("heres the detour. put your code in here");
+
+        return (fn_ptrs.addy)(input_sample_time, cmd);
+    };
+
+    let mut hook = unsafe {
+        CreateMoveDetour.initialize(createmove_hook, closure_for_createmove).unwrap()
+    };
+
+    unsafe {
+        hook.enable().unwrap();
+    }
+
+    createmove_hook(1.0, std::ptr::null_mut()); // call this so hook.call works
+    hook.call(100.0, std::ptr::null_mut());*/
+}
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+pub extern "system" fn DllMain(
+    dll_module: winapi::HINSTANCE,
+    call_reason: winapi::DWORD,
+    _reserved: winapi::LPVOID)
+    -> winapi::BOOL
+{
+    use winapi::um::winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
+
+    match call_reason {
+        DLL_PROCESS_ATTACH => init(),
+        DLL_PROCESS_DETACH => {
+            println!("Detatch");
+        },
+        _ => ()
+    }
+
+    return winapi::TRUE;
+}
