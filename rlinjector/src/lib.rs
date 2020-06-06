7067955076379b2d3313dc50e9d3b09bae2b29a6
Riley Labrecque <git@rileylabrecque.com>
2020-06-06 15:26:36 -0700

    More injection progress

--- rlinjector/src/lib.rs
+++ rlinjector/src/lib.rs
@@ -1,3 +1,3 @@
-pub fn hello_world(process_name: &str) {
-    println!("Hello World {}", process_name);
+pub fn inject(dll_path: &std::path::PathBuf, process_name: &str) {
+    println!("Hello World {:#?} {}", dll_path, process_name);
 }
--- rlinjector/src/main.rs
+++ rlinjector/src/main.rs
@@ -7,11 +7,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Options::default();
 
     if !opt.dll_path.exists() {
-        let err_msg = format!("DLL file specified does not exist: {:?}", opt.dll_path);
+        let err_msg = format!("DLL file specified does not exist: {:#?}", opt.dll_path);
         return Err(err_msg.into());
     }
 
-    lib::hello_world(&opt.process_name);
+    lib::inject(&opt.dll_path, &opt.process_name);
 
     Ok(())
 }
