557010ba5290519be8befb0a3dfe3272117df40d
Riley Labrecque <git@rileylabrecque.com>
2020-06-13 00:20:47 -0700

    Add a ton more variable getters, and fixed some types on others

--- rld2hack/src/d2.rs
+++ rld2hack/src/d2.rs
@@ -1,5 +1,17 @@
 use crate::library::Library;
 
+pub struct AutomapOffset {
+    x: i32,
+    y: i32,
+}
+
+impl std::fmt::Display for AutomapOffset {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+
 pub fn get_screensize_x(game: &Library) -> &u32 {
     unsafe {
         game.read(0x31146Cusize) as &u32
@@ -12,15 +24,15 @@ pub fn get_screensize_y(game: &Library) -> &u32 {
     }
 }
 
-pub fn get_cursor_hover_x(game: &Library) -> &u32 {
+pub fn get_cursor_hover_x(game: &Library) -> &i32 {
     unsafe {
-        game.read(0x321E4Cusize) as &u32
+        game.read(0x321E4Cusize) as &i32
     }
 }
 
-pub fn get_cursor_hover_y(game: &Library) -> &u32 {
+pub fn get_cursor_hover_y(game: &Library) -> &i32 {
     unsafe {
-        game.read(0x321E50usize) as &u32
+        game.read(0x321E50usize) as &i32
     }
 }
 
@@ -36,59 +48,71 @@ pub fn get_mouse_pos_x(game: &Library) -> &u32 {
     }
 }
 
-pub fn get_mouse_offset_y(game: &Library) -> &u32 {
+pub fn get_mouse_offset_y(game: &Library) -> &i32 {
     unsafe {
-        game.read(0x3A5208usize) as &u32
+        game.read(0x3A5208usize) as &i32
     }
 }
 
-pub fn get_mouse_offset_z(game: &Library) -> &u32 {
+pub fn get_mouse_offset_z(game: &Library) -> &i32 {
     unsafe {
-        game.read(0x3A5214usize) as &u32
+        game.read(0x3A5214usize) as &i32
     }
 }
 
-pub fn get_mouse_offset_x(game: &Library) -> &u32 {
+pub fn get_mouse_offset_x(game: &Library) -> &i32 {
     unsafe {
-        game.read(0x3A520Cusize) as &u32
+        game.read(0x3A520Cusize) as &i32
     }
 }
 
-/*
-VARPTR(D2CLIENT, AutomapOn, DWORD, 0x3A27E8)            // Updated 1.14d //007A27E8-BASE
-VARPTR(D2CLIENT, AutomapMode, int, 0x311254)            // Updated 1.14d //00711254-BASE **Divisor**
-VARPTR(D2CLIENT, Offset, POINT, 0x3A5198)               // Updated 1.14d //007A5198-BASE
-VARPTR(D2CLIENT, AutomapLayer, AutomapLayer*, 0x3A5164) // Updated 1.14d //007A5164-BASE
+pub fn get_automap_on(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A27E8usize) as &u32
+    }
+}
 
-// VARPTR(D2CLIENT, MercStrIndex, WORD, 0x325494) //Updated 1.14d //00725494-BASE
-VARPTR(D2CLIENT, MercReviveCost, DWORD, 0x3C0DD0) // Updated 1.14d //007C0DD0-BASE
+pub fn get_automap_mode(game: &Library) -> &i32 {
+    unsafe {
+        game.read(0x311254usize) as &i32
+    }
+}
 
-VARPTR(D2CLIENT, ServerSideUnitHashTables, UnitHashTable, 0x3A5E70) // Updated 1.14d //007A5E70-BASE
-VARPTR(D2CLIENT, ClientSideUnitHashTables, UnitHashTable, 0x3A5270) // Updated 1.14d //007A5270-BASE
+pub fn get_automap_offset(game: &Library) -> &AutomapOffset {
+    unsafe {
+        game.read(0x3A5198usize) as &AutomapOffset
+    }
+}
 
-VARPTR(D2CLIENT, ViewportY, int, 0x3A5208) // Updated 1.14d //007A5208-BASE **MouseOffsetY
-VARPTR(D2CLIENT, ViewportX, int, 0x3A520C) // Updated 1.14d //007A520C-BASE **MouseOffsetX
+pub fn get_viewport_x(game: &Library) -> &i32 {
+    unsafe {
+        game.read(0x3A5208usize) as &i32
+    }
+}
 
-VARPTR(D2CLIENT, GoldDialogAction, DWORD, 0x3A279C) // Updated 1.14d //007A279C-BASE
-VARPTR(D2CLIENT, GoldDialogAmount, DWORD, 0x3A2A68) // Updated 1.14d //007A2A68-BASE
+pub fn get_viewport_y(game: &Library) -> &i32 {
+    unsafe {
+        game.read(0x3A520Cusize) as &i32
+    }
+}
 
-VARPTR(D2CLIENT, NPCMenu, NPCMenu*, 0x326C48)    // Updated 1.14d //00726C48-BASE
-*/
+pub fn get_gold_dialog_action(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A279Cusize) as &u32
+    }
+}
+
+pub fn get_gold_dialog_amount(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A2A68usize) as &u32
+    }
+}
 
 pub fn get_npc_menu_amount(game: &Library) -> &u32 {
     unsafe {
         game.read(0x325A74usize) as &u32
     }
 }
-
-/*
-VARPTR(D2CLIENT, TradeLayout, InventoryLayout*, 0x3BCA30)     // Updated 1.14d //007BCA30-BASE
-VARPTR(D2CLIENT, StashLayout, InventoryLayout*, 0x3BCA78)     // Updated 1.14d //007BCA78-BASE
-VARPTR(D2CLIENT, StoreLayout, InventoryLayout*, 0x3BCB58)     // Updated 1.14d //007BCB58-BASE
-VARPTR(D2CLIENT, CubeLayout, InventoryLayout*, 0x3BCB70)      // Updated 1.14d //007BCB70-BASE
-VARPTR(D2CLIENT, InventoryLayout, InventoryLayout*, 0x3BCB88) // Updated 1.14d //007BCB88-BASE
-VARPTR(D2CLIENT, MercLayout, InventoryLayout*, 0x3BCD4C)      // Updated 1.14d //007BCD4C-BASE
-*/
 
 pub fn get_regular_cursor_type(game: &Library) -> &u32 {
     unsafe {
@@ -131,3 +155,134 @@ pub fn get_divisor(game: &Library) -> &i32 {
         game.read(0x311254usize) as &i32
     }
 }
+
+pub fn get_overhead_trigger(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3BF20Eusize) as &u32
+    }
+}
+
+
+pub fn get_recent_interact_id(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3C0D25usize) as &u32
+    }
+}
+
+pub fn get_item_price_list(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3C0D43usize) as &u32
+    }
+}
+
+pub fn get_waypoint_table(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3BF081usize) as &u32
+    }
+}
+
+pub fn get_is_weapon_swapped(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3BCC4Cusize) as &u32
+    }
+}
+
+pub fn get_is_trade_accepted(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3BCE18usize) as &u32
+    }
+}
+
+pub fn get_is_trade_block(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3BCE28usize) as &u32
+    }
+}
+
+pub fn get_recent_trade_id(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3C0E7Cusize) as &u32
+    }
+}
+
+pub fn get_exp_char_flag(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A04F4usize) as &u32
+    }
+}
+
+pub fn get_map_id(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A0638usize) as &u32
+    }
+}
+
+pub fn get_always_run(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A0660usize) as &u32
+    }
+}
+
+pub fn get_no_pickup(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3A6A90usize) as &u32
+    }
+}
+
+pub fn get_chat_message(game: &Library) -> String {
+    unsafe {
+        let ptr = game.read(0x3BB638usize) as *const u16;
+        if ptr.is_null() {
+            String::new()
+        }
+        else {
+            let widestr = widestring::WideCStr::from_ptr_str(ptr);
+            widestr.to_string_lossy()
+        }
+    }
+}
+
+pub fn get_orifice_id(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3C547Cusize) as &u32
+    }
+}
+
+pub fn get_cursor_item_mode(game: &Library) -> &u32 {
+    unsafe {
+        game.read(0x3C5474usize) as &u32
+    }
+}
+
+/*
+VARPTR(D2CLIENT, AutomapLayer, AutomapLayer*, 0x3A5164) // Updated 1.14d //007A5164-BASE
+VARPTR(D2CLIENT, MercStrIndex, WORD, 0x325494) //Updated 1.14d //00725494-BASE
+
+VARPTR(D2CLIENT, ServerSideUnitHashTables, UnitHashTable, 0x3A5E70) // Updated 1.14d //007A5E70-BASE
+VARPTR(D2CLIENT, ClientSideUnitHashTables, UnitHashTable, 0x3A5270) // Updated 1.14d //007A5270-BASE
+
+VARPTR(D2CLIENT, NPCMenu, NPCMenu*, 0x326C48)    // Updated 1.14d //00726C48-BASE
+
+VARPTR(D2CLIENT, TradeLayout, InventoryLayout*, 0x3BCA30)     // Updated 1.14d //007BCA30-BASE
+VARPTR(D2CLIENT, StashLayout, InventoryLayout*, 0x3BCA78)     // Updated 1.14d //007BCA78-BASE
+VARPTR(D2CLIENT, StoreLayout, InventoryLayout*, 0x3BCB58)     // Updated 1.14d //007BCB58-BASE
+VARPTR(D2CLIENT, CubeLayout, InventoryLayout*, 0x3BCB70)      // Updated 1.14d //007BCB70-BASE
+VARPTR(D2CLIENT, InventoryLayout, InventoryLayout*, 0x3BCB88) // Updated 1.14d //007BCB88-BASE
+VARPTR(D2CLIENT, MercLayout, InventoryLayout*, 0x3BCD4C)      // Updated 1.14d //007BCD4C-BASE
+
+VARPTR(D2CLIENT, TransactionDialog, void*, 0x3C0D63)                           // Updated 1.14d //007C0D63-BASE
+VARPTR(D2CLIENT, TransactionDialogs, DWORD, 0x3C0E5C)                          // Updated 1.14d //007C0E5C-BASE
+VARPTR(D2CLIENT, TransactionDialogs_2, DWORD, 0x3C0E58)                        // Updated 1.14d //007C0E58-BASE
+VARPTR(D2CLIENT, pTransactionDialogsInfo, TransactionDialogsInfo_t*, 0x3C0E54) // Updated 1.14d //007C0E54-BASE
+
+VARPTR(D2CLIENT, GameInfo, GameStructInfo*, 0x3A0438) // Updated 1.14d //007A0438-BASE
+
+VARPTR(D2CLIENT, PlayerUnit, UnitAny*, 0x3A6A70)      // Updated 1.14d //007A6A70-BASE
+VARPTR(D2CLIENT, SelectedInvItem, UnitAny*, 0x3BCBF4) // Updated 1.14d //007BCBF4-BASE
+// VARPTR(D2CLIENT, SelectedUnit, UnitAny*, 0x11C4D8) // unused, but can we use it somewhere maybe? // 1.12 still
+VARPTR(D2CLIENT, PlayerUnitList, RosterUnit*, 0x3BB5C0) // Updated 1.14d //007BB5C0-BASE
+
+// VARPTR(D2CLIENT, RecentTradeName, wchar_t*, 0x12334C)
+
+// VARPTR(D2CLIENT, ScreenCovered, DWORD, 0x1E8F9) // unused, appears to be an int specifying which screens (if any) are opened...
+*/
--- rld2hack/src/lib.rs
+++ rld2hack/src/lib.rs
@@ -86,20 +86,37 @@ fn dll_attach(base: winapi::shared::minwindef::LPVOID) {
 
     print_dbg(&format!("Offset: {:#?}", game.handle));
 
-    for _ in 0..15 {
+    for _ in 0..40 {
+        print_dbg("");
         print_dbg(&format!("ScreenSize: {}x{}", d2::get_screensize_x(&game), d2::get_screensize_y(&game)));
-        print_dbg(&format!("Cursor Hover: {}, {}", d2::get_cursor_hover_x(&game), d2::get_cursor_hover_y(&game)));
-        print_dbg(&format!("Mouse Pos: {}, {}", d2::get_mouse_pos_x(&game), d2::get_mouse_pos_y(&game)));
-        print_dbg(&format!("Mouse Offset: {}, {}, {}", d2::get_mouse_offset_y(&game), d2::get_mouse_offset_z(&game), d2::get_mouse_offset_x(&game)));
+        print_dbg(&format!("Cursor Hover: ({}, {})", d2::get_cursor_hover_x(&game), d2::get_cursor_hover_y(&game)));
+        print_dbg(&format!("Mouse Pos: ({}, {})", d2::get_mouse_pos_x(&game), d2::get_mouse_pos_y(&game)));
+        print_dbg(&format!("Mouse Offset: ({}, {}, {})", d2::get_mouse_offset_y(&game), d2::get_mouse_offset_z(&game), d2::get_mouse_offset_x(&game)));
+        print_dbg(&format!("Automap: On: {} Mode: {} Offset: {}", d2::get_automap_on(&game), d2::get_automap_mode(&game), d2::get_automap_offset(&game)));
+        print_dbg(&format!("Viewport: ({}, {})", d2::get_viewport_x(&game), d2::get_viewport_y(&game)));
+        print_dbg(&format!("Gold Dialog: Action: {} Amount: {}", d2::get_gold_dialog_action(&game), d2::get_gold_dialog_amount(&game)));
+        print_dbg(&format!("NPC Menu Amount: {}", d2::get_npc_menu_amount(&game)));
+        print_dbg(&format!("Regular Cursor Type: {}", d2::get_regular_cursor_type(&game)));
+        print_dbg(&format!("Shop Cursor Type: {}", d2::get_shop_cursor_type(&game)));
         print_dbg(&format!("FPS: {}", d2::get_fps(&game)));
         print_dbg(&format!("Skip: {}", d2::get_skip(&game)));
         print_dbg(&format!("Ping: {}", d2::get_ping(&game)));
         print_dbg(&format!("Lang: {}", d2::get_lang(&game)));
         print_dbg(&format!("Divisor: {}", d2::get_divisor(&game)));
-        print_dbg(&format!("Regular Cursor Type: {}", d2::get_regular_cursor_type(&game)));
-        print_dbg(&format!("Shop Cursor Type: {}", d2::get_shop_cursor_type(&game)));
-        print_dbg(&format!("NPC Menu Amount: {}", d2::get_npc_menu_amount(&game)));
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        print_dbg(&format!("Overhead Trigger: {}", d2::get_overhead_trigger(&game)));
+        print_dbg(&format!("Recent Interact Id: {}", d2::get_recent_interact_id(&game)));
+        print_dbg(&format!("Item Price List: {}", d2::get_item_price_list(&game)));
+        print_dbg(&format!("Waypoint Table: {}", d2::get_waypoint_table(&game)));
+        print_dbg(&format!("Is Weapon Swapped: {}", d2::get_is_weapon_swapped(&game)));
+        print_dbg(&format!("Trade: Accepted: {} Blocked: {} Recent Trade Id: {}", d2::get_is_trade_accepted(&game), d2::get_is_trade_block(&game), d2::get_recent_trade_id(&game)));
+        print_dbg(&format!("Exp Char Flag: {}", d2::get_exp_char_flag(&game)));
+        print_dbg(&format!("Map Id: {}", d2::get_map_id(&game)));
+        print_dbg(&format!("Always Run: {}", d2::get_always_run(&game)));
+        print_dbg(&format!("No Pickup: {}", d2::get_no_pickup(&game)));
+        print_dbg(&format!("Chat Message: {}", d2::get_chat_message(&game)));
+        print_dbg(&format!("Orfice Id: {}", d2::get_orifice_id(&game)));
+        print_dbg(&format!("Cursor Item Mode: {}", d2::get_cursor_item_mode(&game)));
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
 
