d4f2be986075f7678a7a9a1f77afd522f1e774e6
Riley Labrecque <git@rileylabrecque.com>
2020-12-07 22:24:37 -0800

    Fix compile errors

--- rld2hack/src/lib.rs
+++ rld2hack/src/lib.rs
@@ -16,6 +16,8 @@ static mut THREAD_HANDLE: winapi::um::winnt::HANDLE = std::ptr::null_mut();
 fn dll_attach(base: winapi::shared::minwindef::LPVOID) {
     println!("Attach!");
 
+    let game = Library::new("Game.exe".to_owned());
+    let d2core = D2Core::new(game.clone());
 
     while THREAD_RUNNING.load(std::sync::atomic::Ordering::Relaxed) {
         println!("");
@@ -23,103 +25,103 @@ fn dll_attach(base: winapi::shared::minwindef::LPVOID) {
             "ScreenSize: {}x{}",
             d2::variables::get_screensize_x(&game),
             d2::variables::get_screensize_y(&game)
-        ));
+        );
         println!(
             "Cursor Hover: ({}, {})",
             d2::variables::get_cursor_hover_x(&game),
             d2::variables::get_cursor_hover_y(&game)
-        ));
+        );
         println!(
             "Mouse Pos: ({}, {})",
             d2::variables::get_mouse_pos_x(&game),
             d2::variables::get_mouse_pos_y(&game)
-        ));
+        );
         println!(
             "Mouse Offset: ({}, {}, {})",
             d2::variables::get_mouse_offset_y(&game),
             d2::variables::get_mouse_offset_z(&game),
             d2::variables::get_mouse_offset_x(&game)
-        ));
+        );
         println!(
             "Automap: On: {} Mode: {} Offset: {}",
             d2::variables::get_automap_on(&game),
             d2::variables::get_automap_mode(&game),
             d2::variables::get_automap_offset(&game)
-        ));
+        );
         println!(
             "Viewport: ({}, {})",
             d2::variables::get_viewport_x(&game),
             d2::variables::get_viewport_y(&game)
-        ));
+        );
         println!(
             "Gold Dialog: Action: {} Amount: {}",
             d2::variables::get_gold_dialog_action(&game),
             d2::variables::get_gold_dialog_amount(&game)
-        ));
+        );
         println!(
             "NPC Menu Amount: {}",
             d2::variables::get_npc_menu_amount(&game)
-        ));
+        );
         println!(
             "Regular Cursor Type: {}",
             d2::variables::get_regular_cursor_type(&game)
-        ));
+        );
         println!(
             "Shop Cursor Type: {}",
             d2::variables::get_shop_cursor_type(&game)
-        ));
-        println!("FPS: {}", d2::variables::get_fps(&game)));
-        println!("Skip: {}", d2::variables::get_skip(&game)));
-        println!("Ping: {}", d2::variables::get_ping(&game)));
-        println!("Lang: {}", d2::variables::get_lang(&game)));
-        println!("Divisor: {}", d2::variables::get_divisor(&game)));
+        );
+        println!("FPS: {}", d2::variables::get_fps(&game));
+        println!("Skip: {}", d2::variables::get_skip(&game));
+        println!("Ping: {}", d2::variables::get_ping(&game));
+        println!("Lang: {}", d2::variables::get_lang(&game));
+        println!("Divisor: {}", d2::variables::get_divisor(&game));
         println!(
             "Overhead Trigger: {}",
             d2::variables::get_overhead_trigger(&game)
-        ));
+        );
         println!(
             "Recent Interact Id: {}",
             d2::variables::get_recent_interact_id(&game)
-        ));
+        );
         println!(
             "Item Price List: {}",
             d2::variables::get_item_price_list(&game)
-        ));
-        println!("Waypoint Table: {}", d2::variables::get_waypoint_table(&game)));
+        );
+        println!("Waypoint Table: {}", d2::variables::get_waypoint_table(&game));
         println!(
             "Is Weapon Swapped: {}",
             d2::variables::get_is_weapon_swapped(&game)
-        ));
+        );
         println!(
             "Trade: Accepted: {} Blocked: {} Recent Trade Id: {}",
             d2::variables::get_is_trade_accepted(&game),
             d2::variables::get_is_trade_block(&game),
             d2::variables::get_recent_trade_id(&game)
-        ));
-        println!("Exp Char Flag: {}", d2::variables::get_exp_char_flag(&game)));
-        println!("Map Id: {}", d2::variables::get_map_id(&game)));
-        println!("Always Run: {}", d2::variables::get_always_run(&game)));
-        println!("No Pickup: {}", d2::variables::get_no_pickup(&game)));
-        println!("Chat Message: {}", d2::variables::get_chat_message(&game)));
-        println!("Orfice Id: {}", d2::variables::get_orifice_id(&game)));
+        );
+        println!("Exp Char Flag: {}", d2::variables::get_exp_char_flag(&game));
+        println!("Map Id: {}", d2::variables::get_map_id(&game));
+        println!("Always Run: {}", d2::variables::get_always_run(&game));
+        println!("No Pickup: {}", d2::variables::get_no_pickup(&game));
+        println!("Chat Message: {}", d2::variables::get_chat_message(&game));
+        println!("Orfice Id: {}", d2::variables::get_orifice_id(&game));
         println!(
             "Cursor Item Mode: {}",
             d2::variables::get_cursor_item_mode(&game)
-        ));
+        );
 
         println!("");
 
-        println!("Automap Size: {}", d2::functions::get_automap_size(&game)));
-        println!("Difficulty: {}", d2::functions::get_difficulty(&game)));
+        println!("Automap Size: {}", d2::functions::get_automap_size(&game));
+        println!("Difficulty: {}", d2::functions::get_difficulty(&game));
         println!(
             "Game Language Code: {}",
             d2::functions::get_game_language_code(&game)
-        ));
+        );
         println!(
             "Mouse Offset: ({}, {})",
             d2::functions::get_mouse_x_offset(&game),
             d2::functions::get_mouse_y_offset(&game)
-        ));
+        );
         d2::functions::print_game_string(&d2core, "I love you", 0);
         //d2::functions::print_party_string(&game, "Spam", 1);
 
